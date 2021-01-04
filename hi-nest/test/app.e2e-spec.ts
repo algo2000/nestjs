d98b41d25197d5408f9459da026de9dbe5a93df9
leo <algo2000@naver.com>
2021-01-04 23:47:56 +0900

    update Testing GET movies id

--- hi-nest/test/app.e2e-spec.ts
+++ hi-nest/test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    )
     await app.init();
   });
 
@@ -46,5 +53,18 @@ describe('AppController (e2e)', () => {
         .delete('/movies')
         .expect(404)
     })
+  })
+  describe('/movies/:id', ()=>{
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
+    })
+    it('DELETE 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/9999')
+        .expect(404);
+    })
+    it.todo('PATCH')
   })
 });
