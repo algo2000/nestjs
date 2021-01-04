567766cc48bee48b38653b25c9ab65d7e5d85e33
leo <algo2000@naver.com>
2021-01-04 23:54:05 +0900

    update Testing PATCH and DELETE movies id

--- hi-nest/test/app.e2e-spec.ts
+++ hi-nest/test/app.e2e-spec.ts
@@ -30,14 +30,14 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('(GET)', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer())
         .get('/movies')
         .expect(200)
         .expect([])
     })
 
-    it('(POST)', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -47,8 +47,20 @@ describe('AppController (e2e)', () => {
         })
         .expect(201)
     })
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: "Test",
+          year: 2000,
+          genres: ['test'],
+          other: 'thing'
+        })
+        .expect(400)
+    })
     
-    it('(DELETE)', () => {
+    it('DELETE 404', () => {
       return request(app.getHttpServer())
         .delete('/movies')
         .expect(404)
@@ -60,11 +72,23 @@ describe('AppController (e2e)', () => {
         .get('/movies/1')
         .expect(200);
     })
-    it('DELETE 404', () => {
+    it('GET 404', () => {
       return request(app.getHttpServer())
         .get('/movies/9999')
         .expect(404);
     })
-    it.todo('PATCH')
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({title: 'Updated Test'})
+        .expect(200)
+    })
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200)
+    })
   })
 });
