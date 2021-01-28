e314368f2d29aa8754c48eb21bd762090a7ee576
leo <algo2000@naver.com>
2021-01-28 21:59:00 +0900

    update nestjs-example middleware

--- nest-example/src/app.module.ts
+++ nest-example/src/app.module.ts
@@ -1,7 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
+import { logger } from './logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      // .exclude(
+      //   { path: 'cats', method: RequestMethod.GET },
+      //   { path: 'cats', method: RequestMethod.POST },
+      //   'cats/(.*)',
+      // )
+      .forRoutes(CatsController);
+  }
+}
