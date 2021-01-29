9ffefd4134585b573b545910705cba2d1c61a166
leo <algo2000@naver.com>
2021-01-29 23:02:42 +0900

    update nestjs-example pip

--- nest-example/src/cats/cats.controller.ts
+++ nest-example/src/cats/cats.controller.ts
@@ -8,8 +8,10 @@ import {
   Query,
   Redirect,
   UseFilters,
+  UsePipes,
 } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
+import { ValidationPipe } from 'src/validation.pipe';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 
@@ -18,13 +20,17 @@ export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  @UseFilters(HttpExceptionFilter)
-  async create(@Body() createCatDto: CreateCatDto) {
-    console.log(createCatDto.name);
-    console.log(createCatDto.age);
-    console.log(createCatDto.breed);
-    throw new ForbiddenException();
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
   }
+  // @Post()
+  // @UseFilters(HttpExceptionFilter)
+  // async create(@Body() createCatDto: CreateCatDto) {
+  //   console.log(createCatDto.name);
+  //   console.log(createCatDto.age);
+  //   console.log(createCatDto.breed);
+  //   throw new ForbiddenException();
+  // }
 
   @Get()
   async findAll() {
